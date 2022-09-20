d0d505371863352a785640c70ce21c95d3257a46
Cem <cemkgrbz@gmail.com>
2022-09-20 15:51:54 +0200

    added js file

---
+++ index.js
@@ -0,0 +1,35 @@
+//1
+
+const wrapper = document.getElementById("wrapper");
+
+console.log(wrapper.children);
+
+
+for (let index = 0; index < wrapper.children.length; index++) {
+    const element = wrapper.children[index];
+
+    console.log(element.children, element.children.length);
+}
+
+//2
+
+const getElementWithMostClasses = (list) => {
+    let el = null;
+    let maxLength = 0;
+
+    for (let index = 0; index < list.length; index++) {
+        const element = list[index];
+
+        if (element.classList.length > maxLength) {
+            maxLength = element.classList.length;
+            el = element;
+        }
+    }
+
+    return {
+        element: el,
+        numberOfClasses: maxLength
+    };
+}
+
+console.log(getElementWithMostClasses(wrapper.children));
